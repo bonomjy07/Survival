b6cd1131ce63f4c75a4e24bf07ecce8d1a8325a7
BHY <inomp3042@naver.com>
2020-02-06 19:33:56 +0900

    add test resource files

---
+++ Resources/res/TestResource/TileMap/block_tileset.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="block_tileset" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <properties>
+  <property name="collision" type="bool" value="false"/>
+ </properties>
+ <image source="../TileImage/img_block.png" width="67" height="34"/>
+ <tile id="0">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ Resources/res/TestResource/TileMap/water_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="water_tileset" tilewidth="32" tileheight="32" tilecount="72" columns="9">
+ <image source="../TileImage/img_water.png" width="288" height="256"/>
+</tileset>
