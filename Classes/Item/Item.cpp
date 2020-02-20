73c8aa4d6b99dbad27c30701ab222768567d9586
BHY <inomp3042@naver.com>
2020-02-20 15:15:41 +0900

    Split ItemSprite class

--- Classes/Item/Item.cpp
+++ Classes/Item/Item.cpp
@@ -1,7 +1,6 @@
 
 #include "Item.h"
 
-// def item class
 Item::Item(const char * _des, const char * _img,
      const char * _thumb, const int _weight)
     :description(_des), imageFileName(_img),
@@ -29,38 +28,3 @@ const char * Item::getThumbnailFileName()
 {
     return this->thumbnailFileName;
 }
-
-// item class
-
-
-// def ItemSprite class
-ItemSprite::ItemSprite(){}
-
-ItemSprite::~ItemSprite()
-{
-    item->release();
-}
-
-bool ItemSprite::init()
-{
-    if(!Sprite::init()){
-        return false;
-    }
-    return true;
-}
-
-Item * ItemSprite::getItem()
-{
-    return this->item;
-}
-void ItemSprite::setItem(Item *_item)
-{
-    this->item = _item;
-    this->setItemTexture();
-}
-void ItemSprite::setItemTexture(){
-    if(this->item){
-        this->setTexture(this->item->getImageFileName());
-    }
-}
-// ItemSprite class
---
+++ Classes/Sprites/ItemSprite.cpp
@@ -0,0 +1,45 @@
+
+#include "ItemSprite.h"
+
+USING_NS_CC;
+
+ItemSprite::ItemSprite(){}
+
+ItemSprite::~ItemSprite()
+{
+    if( item )
+        item->release();
+}
+
+bool ItemSprite::init()
+{
+    if(!Sprite::init()){
+        return false;
+    }
+    item = nullptr;
+    return true;
+}
+
+Item * ItemSprite::getItem()
+{
+    return this->item;
+}
+
+void ItemSprite::setItem(Item *_item)
+{
+    this->item = _item;
+    this->item->retain();
+    this->setItemTexture();
+}
+
+void ItemSprite::setItemTexture()
+{
+    if(this->item){
+        this->setTexture(this->item->getImageFileName());
+    }
+}
+
+void ItemSprite::wasCollected()
+{
+    log("ItemSprite: wasCollected()");
+}
