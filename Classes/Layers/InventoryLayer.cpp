c2452aa558debc5f33de35ed2c18d459e4538dab
BHY <inomp3042@naver.com>
2020-02-21 23:23:43 +0900

    Initailize InventoryLayer class

--- Classes/Layers/InventoryLayer.cpp
+++ Classes/Layers/InventoryLayer.cpp
@@ -9,3 +9,50 @@
 //#include "InventoryLayer.hpp"
 
 #include "InventoryLayer.h"
+#include "GridView.h"
+#include "ui/CocosGUI.h"
+
+USING_NS_CC;
+
+InventoryLayer::~InventoryLayer(){}
+
+bool InventoryLayer::init()
+{
+    if ( !Layer::init() )
+        return false;
+    this->setName("InventoryLayer");
+
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+
+    gridView = GridView::create();
+    
+    gridView->setGrid(4,8);
+    // position the label on the center of the screen
+    gridView->setBackGroundColor(Color3B(160, 160, 160));
+    gridView->setBackGroundColorType(ui::Layout::BackGroundColorType::SOLID);
+    for(int i=0; i<gridView->getMaxCount(); i++){
+        auto layout = ui::Layout::create();
+        layout->setLayoutType(ui::Layout::Type::HORIZONTAL);
+        layout->setSize(Size(50, 50));
+        layout->setBackGroundColor(Color3B(200, 200, 200));
+        layout->setBackGroundColorType(ui::Layout::BackGroundColorType::SOLID);
+        gridView->addChild(layout);
+    }
+    gridView->setAnchorPoint(Point(0.5f,0.5f));
+    gridView->setPosition(Point(visibleSize.width/2, visibleSize.height*2/3));
+    gridView->arrange();
+    this->addChild(gridView);
+    // moveCenter();
+    return true;
+}
+
+float InventoryLayer::getWeight() const { return weight; }
+void InventoryLayer::setWeight(float _weight){ weight = _weight; }
+
+void InventoryLayer::moveCenter()
+{
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    setContentSize(visibleSize);
+    auto gridSize = gridView->getContentSize();
+    // this->setPosition(visibleSize.width/2 - gridSize.width/2, visibleSize.height/2);
+}
--- Classes/Layers/InventoryLayer.h
+++ Classes/Layers/InventoryLayer.h
@@ -14,11 +14,14 @@
 
 #include "cocos2d.h"
 
+#include "GridView.h"
+
 #endif /* InventoryLayer_h */
 
 class InventoryLayer : public cocos2d::Layer
 {
 public:
+    virtual ~InventoryLayer();
     virtual bool init() override;
     CREATE_FUNC(InventoryLayer);
     
@@ -27,8 +30,12 @@ public:
     
     float getWeight() const;
     void setWeight(float deltaWeight);
+
+private:
+    void moveCenter();
     
 private:
     //cocos2d::Vector<class Item> inventoryItems;
     float weight;
+    GridView *gridView;
 };
