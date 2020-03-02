62a73fb46af223d7512a74bf234c02c119ff4d5b
BHY <inomp3042@naver.com>
2020-03-02 14:42:39 +0900

    Move network functions of GameLayer to Multi

--- Classes/Network/Multi.cpp
+++ Classes/Network/Multi.cpp
@@ -7,16 +7,56 @@
 
 USING_NS_CC;
 
-Multi::Multi(){
-    init();
-}
+Multi::Role Multi::ROLE_STATUS = Multi::Role::None;
+std::string Multi::SOCKET_ID = "";
+
+Multi::Multi(){}
 Multi::~Multi(){}
 
+static Multi* create(std::string uri) {
+    Multi *pRet = new(std::nothrow) Multi();
+    if (pRet && pRet->init(uri)) {
+        pRet->autorelease();
+        return pRet;
+    }
+    else {
+        delete pRet;
+        pRet = nullptr;
+        return nullptr;
+    }
+}
+
 bool Multi::init(){
-    _client = SocketIO::connect("localhost:3000", *this);
+    if ( !(_client = SocketIO::connect("localhost:8080", *this)) ){
+        return false;
+    }
     _client->on("hello", CC_CALLBACK_2(Multi::onReceiveEvent, this));
     _client->on("action", CC_CALLBACK_2(Multi::onReceiveEvent2, this));
-    player2 = nullptr;
+
+    _client->on("requestPlayerID", CC_CALLBACK_2(Multi::onRequestPlayerID, this));
+    _client->on("newPlayer", CC_CALLBACK_2(Multi::onNewPlayer, this));
+    _client->on("playerList", CC_CALLBACK_2(Multi::onPlayerList, this));
+    _client->on("pawnMove", CC_CALLBACK_2(Multi::onPawnMove, this));
+    _client->on("movePressed", CC_CALLBACK_2(Multi::onMovePressed, this));
+    _client->on("moveReleased", CC_CALLBACK_2(Multi::onMoveReleased, this));
+    _player2 = nullptr;
+    return true;
+}
+
+bool Multi::init(std::string uri){
+    if ( !(_client = SocketIO::connect(uri, *this)) ){
+        return false;
+    }
+    _client->on("hello", CC_CALLBACK_2(Multi::onReceiveEvent, this));
+    _client->on("action", CC_CALLBACK_2(Multi::onReceiveEvent2, this));
+
+    _client->on("requestPlayerID", CC_CALLBACK_2(Multi::onRequestPlayerID, this));
+    _client->on("newPlayer", CC_CALLBACK_2(Multi::onNewPlayer, this));
+    _client->on("playerList", CC_CALLBACK_2(Multi::onPlayerList, this));
+    _client->on("pawnMove", CC_CALLBACK_2(Multi::onPawnMove, this));
+    _client->on("movePressed", CC_CALLBACK_2(Multi::onMovePressed, this));
+    _client->on("moveReleased", CC_CALLBACK_2(Multi::onMoveReleased, this));
+    _player2 = nullptr;
     return true;
 }
 
@@ -29,9 +69,11 @@ void Multi::onMessage(SIOClient* client, const std::string& data){
 }
 void Multi::onClose(SIOClient* client){
     // SocketIO::disconnect success
+    cocos2d::log("onClose()");
 }
 void Multi::onError(SIOClient* client, const std::string& data){
     // SocketIO::failed
+    cocos2d::log("onError : %s", data.c_str());
 }
 
 void Multi::onReceiveEvent(SIOClient* client , const std::string& data){
@@ -40,14 +82,14 @@ void Multi::onReceiveEvent(SIOClient* client , const std::string& data){
     rapidjson::Document doc;
     doc.Parse(data.c_str());
     if ( isMine ) {
-        player_number = doc["value"].GetInt64();
-        log(player_number);
+        _player_number = doc["value"].GetInt64();
+        log(_player_number);
         isMine = false;
     }
     else{
         auto director = Director::getInstance();
         TestScene* testscene = static_cast<TestScene*>(director->getRunningScene()->getChildByName("GameLayer"));
-        player2 = static_cast<SurvivorSprite*>(testscene->getChildByName("player2"));
+        _player2 = static_cast<SurvivorSprite*>(testscene->getChildByName("player2"));
     }
     log("onReceiveEvent");
 }
@@ -57,13 +99,130 @@ void Multi::onReceiveEvent2(SIOClient* client , const std::string& data){
     rapidjson::Document doc;
     doc.Parse(data.c_str());
     if ( !std::string("2").compare(doc["player"].GetString()) ) {
-        if ( player2 )
-            player2->collect();
+        if ( _player2 )
+            _player2->collect();
     }
     log("onReceiveEvent2");
 }
 
 void Multi::sendText(){
-    _client->emit("action", "[{\"player\": \"" + std::to_string(player_number) + "\",\"value\":\"hihihihih\"}]");
+    _client->emit("action", "[{\"player\": \"" + std::to_string(_player_number) + "\",\"value\":\"hihihihih\"}]");
     log("sendText");
+}
+
+void Multi::onRequestPlayerID(cocos2d::network::SIOClient* client, const std::string& data)
+{
+    rapidjson::Document document;
+    document.Parse(data.c_str());
+    if (!document.GetParseError())
+    {
+        // Get ID and check if i'm the host
+        SOCKET_ID = document["MyID"].GetString();
+        if (SOCKET_ID.compare(document["HostID"].GetString()) == 0)
+        {
+            ROLE_STATUS = Role::Host;
+        }
+        else
+        {
+            ROLE_STATUS = Role::Client;
+        }
+    }
+}
+
+void Multi::onNewPlayer(cocos2d::network::SIOClient* client, const std::string& data)
+{
+    GameLayer* layer = getParentLayer();
+
+    // Get my ID
+    rapidjson::Document document;
+    document.Parse(data.c_str());
+    if (!document.GetParseError())
+    {
+        std::string ID = document["ID"].GetString();
+        layer->addPlayerSpriteInWorld(ID, {336, 368});
+    }
+    // Broadcasts player list to all clients
+    std::string playerList = "[";
+    for (auto player : layer->getPlayersManager())
+    {
+        std::string ID, x, y, data;
+        ID = ID+"\"ID\":"+"\""+player.first+"\"";
+        x = x+"\"x\":"+std::to_string(player.second->getPositionX());
+        y = y+"\"y\":"+std::to_string(player.second->getPositionY());
+        data = data+"{"+ID+","+x+","+y+"}";
+        playerList += data + ",";
+    }
+    playerList.erase(playerList.length()-1);
+    playerList += "]";
+    _client->emit("playerList", playerList);
+}
+
+void Multi::onPlayerList(cocos2d::network::SIOClient *client, const std::string &data)
+{
+    auto layer = getParentLayer();
+
+    rapidjson::Document document;
+    document.Parse(data.c_str());
+    if (!document.GetParseError())
+    {
+        for (const auto& ele : document.GetArray())
+        {
+            std::string ID = ele["ID"].GetString();
+            float x = ele["x"].GetFloat();
+            float y = ele["y"].GetFloat();
+            layer->addPlayerSpriteInWorld(ID, {x,y});
+        }
+    }
+}
+
+void Multi::onPawnMove(cocos2d::network::SIOClient* client, const std::string& data)
+{
+    auto layer = getParentLayer();
+
+    if (Multi::Role::Client == ROLE_STATUS)
+    {
+        rapidjson::Document document;
+        document.Parse(data.c_str());
+        if (!document.GetParseError())
+        {
+            std::string ID = document["ID"].GetString();
+            Vec2 newPosition(document["x"].GetFloat(), document["y"].GetFloat());
+            auto playerSprite = layer->getPlayerSprite(ID);
+            playerSprite->moveThePawn(newPosition);
+        }
+    }
+}
+
+void Multi::onMovePressed(cocos2d::network::SIOClient *client, const std::string &data)
+{
+    auto layer = getParentLayer();
+
+    // get direction
+    rapidjson::Document document;
+    document.Parse(data.c_str());
+    if (!document.GetParseError())
+    {
+        auto id = document["ID"].GetString();
+        auto direction = document["direction"].GetString();
+        layer->onMovePressed(id, direction);
+    }
+}
+
+void Multi::onMoveReleased(cocos2d::network::SIOClient* client, const std::string& data)
+{
+    auto layer = getParentLayer();
+
+    // get direction
+    rapidjson::Document document;
+    document.Parse(data.c_str());
+    if (!document.GetParseError())
+    {
+        auto id = document["ID"].GetString();
+        auto direction = document["direction"].GetString();
+        layer->onMoveReleased(id, direction);
+    }
+}
+
+GameLayer* Multi::getParentLayer(){
+    return static_cast<GameLayer*>(getParent());
 }
--- Classes/Network/Multi.h
+++ Classes/Network/Multi.h
@@ -7,27 +7,59 @@
 USING_NS_CC;
 using namespace cocos2d::network;
 
-class Multi : public Ref, public SocketIO::SIODelegate
+class Multi : public Node, public SocketIO::SIODelegate
 {
+public:
+    enum class Role {None=0, Host, Client};
+    static Role ROLE_STATUS;
+    static std::string SOCKET_ID;
+
 public:
     Multi();
     virtual ~Multi();
     virtual bool init();
+    virtual bool init(std::string uri);
     CREATE_FUNC(Multi);
-
-    virtual void onConnect(SIOClient* client);
-    virtual void onMessage(SIOClient* client, const std::string& data);
-    virtual void onClose(SIOClient* client);
-    virtual void onError(SIOClient* client, const std::string& data);
+    static Multi* create(std::string uri);
 
     void onReceiveEvent(SIOClient* client , const std::string& data);
     void onReceiveEvent2(SIOClient* client , const std::string& data);
     void sendText();
 
+public:
+    /* Gets ID and Checks what role is */
+    void onRequestPlayerID(cocos2d::network::SIOClient* client, const std::string& data);
+    
+    /* Adds new player to player list and braodcasts player list */
+    void onNewPlayer(cocos2d::network::SIOClient* client, const std::string& data);
+    
+    /* Receives player list and create pawn about player list */
+    void onPlayerList(cocos2d::network::SIOClient* client, const std::string& data);
+    
+    /* Broadcasts pawn's movement */
+    void onPawnMove(cocos2d::network::SIOClient* client, const std::string& data);
+    
+    /* Send movement to host */
+    void onMovePressed(cocos2d::network::SIOClient* client, const std::string& data);
+    
+    /* Send movement to host */
+    void onMoveReleased(cocos2d::network::SIOClient* client, const std::string& data);
+
+    virtual void onConnect(SIOClient* client);
+    virtual void onMessage(SIOClient* client, const std::string& data);
+    void onClose(cocos2d::network::SIOClient* client) override;
+    void onError(cocos2d::network::SIOClient* client, const std::string& data) override;
+
+    cocos2d::network::SIOClient* getClient(){ return _client; }
+    void setClient(cocos2d::network::SIOClient* client){ _client = client; }
+
 private:
-    int player_number;
+    class GameLayer* getParentLayer();
+
+private:
+    int _player_number;
     SIOClient* _client;
-    class SurvivorSprite* player2;
+    class SurvivorSprite* _player2;
 };
 
 #endif // MULTI_H__
