799feb2ce2e744079b63ae573282a5391643ba81
BHY <inomp3042@naver.com>
2020-03-04 14:19:58 +0900

    Resize window and show all

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -85,7 +85,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("Survival", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("Survival", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height),1.5F, true);
 #else
         glview = GLViewImpl::create("Survival");
 #endif
@@ -99,7 +99,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     director->setAnimationInterval(1.0f / 60);
 
     // Set the design resolution
-    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+    glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::SHOW_ALL);
     auto frameSize = glview->getFrameSize();
     // if the frame's height is larger than the height of medium size.
     // if (frameSize.height > mediumResolutionSize.height)
