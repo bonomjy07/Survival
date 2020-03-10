ecfaad2445d6e066e61ac80d657a8f9fe60ddf5f
BHY <inomp3042@naver.com>
2020-03-10 16:30:41 +0900

    Add new CREATE_FUNC for making ItemSprite easily

--- Classes/Sprites/ItemSprite.cpp
+++ Classes/Sprites/ItemSprite.cpp
@@ -1,14 +1,17 @@
 
 #include "ItemSprite.h"
 #include "UnitSprite.h"
+#include "Food.h"
+#include "Tool.h"
+
 USING_NS_CC;
 
 ItemSprite::ItemSprite(){}
 
 ItemSprite::~ItemSprite()
 {
-    if( item )
-        item->release();
+    if( _item )
+        _item->release();
 }
 
 bool ItemSprite::init()
@@ -16,39 +19,68 @@ bool ItemSprite::init()
     if(!Sprite::init()){
         return false;
     }
-    item = nullptr;
+    _item = nullptr;
+    return true;
+}
+bool ItemSprite::init(const std::string &itemtype)
+{
+    if(!init()){
+        return false;
+    }
+    Item *item = makeItemByType(itemtype);
+    setItem(item);
+    return true;
+}
+bool ItemSprite::init(Item *item)
+{
+    if(!init()){
+        return false;
+    }
+    setItem(item);
     return true;
 }
 
 Item * ItemSprite::getItem()
 {
-    return this->item;
+    return this->_item;
 }
 
-void ItemSprite::setItem(Item *_item)
+void ItemSprite::setItem(Item *item)
 {
-    if( this->item )
-        this->item->release();
-    _item->retain();
-    this->item = _item;
+    if( this->_item )
+        this->_item->release();
+    item->retain();
+    this->_item = item;
     this->setItemTexture();
 }
 
 void ItemSprite::setItemTexture()
 {
-    if(this->item){
-        this->setTexture(this->item->getImageFileName());
+    if(this->_item){
+        this->setTexture(this->_item->getImageFileName());
         setContentSize(Size(32,32));
     }
 }
 
 void ItemSprite::wasCollected()
 {
-    log("ItemSprite: Item %s was collected", item->getName());
+    removeFromParent();
+    log("ItemSprite: Item %s was collected", _item->getName());
 }
 
 void ItemSprite::wasUsed(UnitSprite *toUnit)
 {
-    item->use(toUnit);
-    log("ItemSprite: Item %s was used", item->getName());
+    _item->use(toUnit);
+    log("ItemSprite: Item %s was used", _item->getName());
+}
+
+Item *ItemSprite::makeItemByType(const std::string &itemtype){
+    Item *item = nullptr;
+    if (!itemtype.compare("DeerMeat")){
+        item = DeerMeat::create();
+    }
+    else if (!itemtype.compare("Sword")){
+        item = Sword::create();
+    }
+    return item;
 }
--- Classes/Sprites/ItemSprite.h
+++ Classes/Sprites/ItemSprite.h
@@ -3,6 +3,7 @@
 
 #include "Item.h"
 #include "MySprite.h"
+#include "Create.h"
 
 class ItemSprite : public MySprite
 {
@@ -10,16 +11,21 @@ public:
     ItemSprite();
     virtual ~ItemSprite();
     virtual bool init();
+    virtual bool init(const std::string &itemtype);
+    virtual bool init(Item *item);
     CREATE_FUNC(ItemSprite);
+    CREATE_FUNC_1(ItemSprite, Item *);
+    CREATE_FUNC_1(ItemSprite, const std::string &);
 
+    static Item *makeItemByType(const std::string &itemtype);
     Item * getItem();
-    void setItem(Item *_item);
+    void setItem(Item *item);
 
     void setItemTexture();
     void wasCollected();
     void wasUsed(class UnitSprite*);
 protected:
-    Item *item;
+    Item *_item;
 };
 
 #endif //ITEM_SPRITE_H__
